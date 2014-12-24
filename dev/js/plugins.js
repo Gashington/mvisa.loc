660b0af5e1e3f23b22b8b0cc164be97ca98840cc
Gashington <gashington@gmail.com>
2014-12-24 16:56:16 +0200

    first

---
+++ dev/js/plugins.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('.js-radio').styler();
+
+    $('.js-fancybox').fancybox({
+        maxWidth	: 740,
+        maxHeight	: 491,
+        fitToView	: false,
+        autoSize	: true,
+        closeClick	: false,
+        padding: 0
+    });
+})
